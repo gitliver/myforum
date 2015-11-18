64cb64ac5b8c2195fe7d48877aa62fa19eccc974
gitliver <oe2118@cumc.columbia.edu>
2015-11-18 22:31:04 +0000

    Threads list updates automatically without page refresh

--- mysite/forum/views.py
+++ mysite/forum/views.py
@@ -22,33 +22,16 @@ def index(request):
     """Main function that gets called when the user lands on the webpage"""
 
     # get forum thread objects, ordered by publication date
-    forumthreads = Thread.objects.order_by('pub_date')
-
-    # this code block uses a page refresh after form submission
-    # instead, we're going to use ajax
-
-    # from: https://docs.djangoproject.com/en/1.8/topics/forms/
-    # if this is a POST request we need to process the form data
-    # if request.method == 'POST':
-    #     # create a form instance and populate it with data from the request:
-    #     form = ThreadModelForm(request.POST)
-    #     # check whether it's valid:
-    #     if form.is_valid():
-    #         # process the data in form.cleaned_data as required
-    #         # ...
-    #         # redirect to a new URL:
-    #         return HttpResponseRedirect('/thanks/')
-    # if a GET (or any other method) we'll create a blank form
-    # else:
-    #    form = ThreadModelForm()
-
-    form = ThreadModelForm()
+    # forumthreads = Thread.objects.order_by('pub_date')
+    # get Thread form
+    # form = ThreadModelForm()
 
     # render the page, passing a dict that holds the form as well as all threads
-    return render(request, 'forum/index.html', {'myform': form, 'mythreads': forumthreads})
+    # return render(request, 'forum/index.html', {'myform': form, 'mythreads': forumthreads})
+    return render(request, 'forum/index.html')
 
 def create_post(request):
-    """Function to create thread (or post), called by javascript AJAX ... I mean, called by Angular"""
+    """Function to create thread (or post), called by Angular"""
 
     # see: http://django-angular.readthedocs.org/en/latest/angular-model-form.html
     if not request.is_ajax():
@@ -62,33 +45,6 @@ def create_post(request):
     post.save()
 
     return JsonResponse(in_data)
-
-    # modified from: https://realpython.com/blog/python/django-and-ajax-form-submissions/
-#    if request.method == 'POST':
-#
-#        # get data
-#        mytitle = request.POST.get('mytitle')
-#        myusername = request.POST.get('myusername')
-#        mydescription = request.POST.get('mydescription')
-#
-#        # save in database
-#        # post = Thread(pub_date = datetime.datetime.now(pytz.timezone('US/Eastern')), username = myusername, title = mytitle, description = mydescription)
-#        # post.save()
-#
-#        # create response data dict
-#        response_data = {
-#			    'title': mytitle,
-#			    'description': mydescription,
-#			    'username': myusername,
-#			}
-#
-#        # return HttpResponse( json.dumps(response_data), content_type="application/json")
-#        return JsonResponse(response_data)
-#
-#    else:
-#    
-#        # return HttpResponse( json.dumps({"nothing": "fail"}), content_type="application/json")
-#        return JsonResponse({"nothing": "to see here!"})
 
 # Serializer classes for REST api
 # modified from http://www.django-rest-framework.org/tutorial/quickstart/
--- mysite/static/js/myAppController.js
+++ mysite/static/js/myAppController.js
@@ -1,16 +1,21 @@
 // Instantiate the Angular app
 var myApp = angular.module('myApp', []);
 
-// This controller will get JSON data for the forum threads via the /threads URL
+// Controller for listing threads and submitting threads via form
 myApp.controller('threadController', function($scope, $http) {
-    $http.get("/threads").success(function(response) {$scope.myThreads = response;});
-});
 
-// This controller will submit the user-provided data in the Create Thread form
-// to the URL /create_post/, at which point the Django view will take over 
-// to add this information into the database
-// modified from http://django-angular.readthedocs.org/en/latest/angular-model-form.html
-myApp.controller('threadFormController', function($scope, $http) {
+    // function to get JSON data for the forum threads via the /threads URL
+    $scope.list = function() {
+        $http.get("/threads").success(function(response) {$scope.myThreads = response;});
+    };
+
+    // call it!
+    $scope.list();
+
+    // This function will submit the user-provided data in the Create Thread form
+    // to the URL /create_post/, at which point the Django view will take over 
+    // to add this information into the database
+    // modified from http://django-angular.readthedocs.org/en/latest/angular-model-form.html
     $scope.submit = function() {
 
 	// console.log($scope.user)
@@ -18,8 +23,8 @@ myApp.controller('threadFormController', function($scope, $http) {
 	// POST $scope.user JSON to create_post/ URL
         $http.post('create_post/', $scope.user)
 	    .success(function(out_data) {
-	        console.log("Success!")
-	        // console.log(out_data)
+		// re-list the threads, since updates have happened
+                $scope.list();
             })
             .error(function (data, status, header, config) {
                 $scope.ResponseDetails = {
@@ -38,6 +43,7 @@ myApp.controller('threadFormController', function($scope, $http) {
 
         $scope.reset();
     };
+
 });
 
 // Add stuff to header, deal with csrftoken issue
